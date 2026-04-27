88b0bb561d3b39714a1f2d777fd17a047b530f3a
tfs2k24-tech <tfs2k24@gmail.com>
2026-04-27 14:38:07 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,8 +2,6 @@ import app from "../app.js";
 import { connectToDatabase } from "../lib/db.js";
 import serverless from "serverless-http";
 
-// connect DB
 await connectToDatabase();
 
-// export handler
 export default serverless(app);
